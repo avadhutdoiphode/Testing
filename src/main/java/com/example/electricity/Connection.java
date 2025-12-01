f246c3ec4da5e8fa48165968a84ae6d6f3f41afd
Admin <avadhutdoiphode1@gmail.com>
2025-12-01 16:36:19 +0530

    one more testing

--- src/main/java/com/example/electricity/Connection.java
+++ src/main/java/com/example/electricity/Connection.java
@@ -6,6 +6,8 @@ public class Connection {
     private BigInteger consumerNumber;
     private int billingUnit;
     private String type;
+    private String name;
+    private String home;
 
     public BigInteger getConsumerNumber() { return consumerNumber; }
     public void setConsumerNumber(BigInteger consumerNumber) { this.consumerNumber = consumerNumber; }
